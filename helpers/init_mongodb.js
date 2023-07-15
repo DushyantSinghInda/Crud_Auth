dd9d87e0170ac993db76644fafd3444a973d5bed
Dushyant <singhdushyant2989@gmail.com>
2023-07-15 17:23:48 +0530

    Joi validation implemented

---
+++ helpers/init_mongodb.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URI, { dbName: process.env.DB_NAME })
+  .then(() => {
+    console.log("MongoDB Connected.");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose Connected to DB.");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err.message);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose Connection is Disconnected.");
+});
+
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  process.exit(0);
+});
--- routes/Auth.route.js
+++ routes/Auth.route.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const createErrorHandler = require("http-errors");
+const User = require("../Models/User.model");
+const { authSchema } = require("../helpers/validation_schema");
 
 router.post("/register", async (req, res, next) => {
-  res.send("Register Route");
+  try {
+    // const { email, password } = req.body;
+    // if (!email || !password) throw createErrorHandler.BadRequest();
+
+    const result = await authSchema.validateAsync(req.body);
+
+    const doesExist = await User.findOne({ email: result.email });
+    if (doesExist)
+      throw createErrorHandler.Conflict(
+        `${result.email} is already been registered`
+      );
+
+    const user = new User(result);
+    const savedUser = await user.save();
+
+    res.send(savedUser);
+  } catch (error) {
+    if (error.isJoi === true) error.status = 422;
+    next(error);
+  }
 });
 
 router.post("/login", async (req, res, next) => {
